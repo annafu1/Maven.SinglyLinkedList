08bc60001752a1dde05e61db133ff4ee95d86c9c
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-11-26 16:07:03 -0500

    added in tests

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,37 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<I extends Number> {
+    public Integer addNode(int i) {
+        return null;
+    }
+
+    public Integer containsNode(int i) {
+        return null;
+    }
+
+    public Integer removeNode(I i) {
+        return null;
+    }
+
+    public Integer findNode(I i) {
+        return null;
+    }
+
+    public Integer sizeNode(int i) {
+        return null;
+    }
+
+
+    public I getNode(I i) {
+        return null;
+    }
+
+    public I copyNode(I i) {
+        return null;
+    }
+
+    public I sortNode(I i) {
+        return null;
+    }
 }
